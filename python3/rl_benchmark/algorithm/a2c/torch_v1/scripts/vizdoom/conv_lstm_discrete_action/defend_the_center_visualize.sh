67404687fe80c958246fec8f72d2ed5c74712cea
k <junchiliang@163.com>
2019-10-12 11:01:09 -0400

    visualize a2c at defend_the_center

---
+++ python3/rl_benchmark/algorithm/a2c/torch_v1/scripts/vizdoom/conv_lstm_discrete_action/defend_the_center_visualize.sh
@@ -0,0 +1,9 @@
+python3 test.py --num_repeat 1\
+ --model_config defend_the_center.a2c.json\
+ --task_file ~/robotics_workspace/vizdoom_experiment/scenarios/defend_the_center.cfg\
+ --task defend_the_center\
+ --test_episode 50\
+ --gpu_id 0\
+ --model_file ~/robotics_workspace/rl_benchmark/trained_model/python3/a2c_defend_the_center_v0_it_3000000.pth\
+ --visualize_pause 10 --visualize_height 360 --visualize_width 480
+
